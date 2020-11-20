d951d7a3628ff007225cad11ade86a863e00ae45
guilhermeepsilva <guilherme.silva@meuacerto.com.br>
2020-11-20 16:44:46 -0300

    Altera mensagem de bem vindo

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Código31</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Seja bem Vindo à <span className={styles.codigo31}>Código31</span>
+          Seja bem vindo à <span className={styles.codigo31}>Código31</span>
         </h1>
 
         <p className={styles.description}>
