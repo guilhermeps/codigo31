dbbf77939a569d50331506484b3c9b34a2ff3a28
guilhermeepsilva <guilherme.silva@meuacerto.com.br>
2020-11-20 18:37:16 -0300

    Altera texto de boas vindas

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Seja bem vindo à <span className={styles.codigo31}>Código31</span>
+          Seja bem-vindo à <span className={styles.codigo31}>Código31</span>
         </h1>
 
         <p className={styles.description}>
